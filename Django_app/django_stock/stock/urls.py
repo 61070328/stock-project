b8a2a35185d1dafba5a9b7d8039cfb80f10fa6d9
RachenKeawngam <42955444+RachenKeawngam@users.noreply.github.com>
2021-10-18 23:22:07 +0700

    Update urls.py

--- Django_app/django_stock/stock/urls.py
+++ Django_app/django_stock/stock/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from . import views   #ชื่อฟังชั่นที่สร้างไว้ใน views.py
 urlpatterns = [
-    path('stock/',views.finance_chart, name='Overview'), #our-domain.com/stock
+    path('stock/',views.symbol_company, name='Overview'), #our-domain.com/stock
     path('stock/chart',views.chart),
     path('stock/<Symbol>/finance_chart/', views.finance_chart, name='finance_chart'),
     # path('stock/<slug:Symbol>/finance-chart/', views.finance_chart, name='finance-chart')
